19f45f1acb685976fce21003778cb49705d44c4e
niko <nikomatutina@gmail.com>
2018-10-09 12:30:28 +0800

    first commit
    added sass
    setup project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot([
+    { path: 'home', component: HomeComponent},
+    { path: '', redirectTo: 'home', pathMatch: 'full'},
+  ])],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
